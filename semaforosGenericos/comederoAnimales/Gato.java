32ccf4b349340828803a7dbdd375ef416e013004
Agustinicolas <agustinicolas@outlook.com>
2022-10-21 17:04:48 -0300

    Implementacion parcial clases perro y gato

---
+++ semaforosGenericos/comederoAnimales/Gato.java
@@ -0,0 +1,20 @@
+package semaforosGenericos.comederoAnimales;
+
+public class Gato implements Runnable {
+
+    private Comedero comedero;
+
+    public Gato(Comedero comedero){
+        this.comedero = comedero;
+    }
+
+    public void run(){
+
+        //  GATO INTENTA ENTRAR COMEDERO
+
+        //  GATO COME
+
+        //  GATO SALE COMEDERO
+    }
+    
+}
---
+++ semaforosGenericos/comederoAnimales/Perro.java
@@ -0,0 +1,19 @@
+package semaforosGenericos.comederoAnimales;
+
+public class Perro implements Runnable {
+    
+    private Comedero comedero;
+
+    public Perro(Comedero comedero){
+        this.comedero = comedero;
+    }
+
+    public void run(){
+
+        //  PERRO INTENTA ENTRAR A COMEDERO
+
+        //  PERRO COME
+
+        //  PERRO SALE COMEDERO
+    }
+}
